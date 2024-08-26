1a74a0164c695c3c04d23de4357c27d808b0bd7d
mohammadrahman <obaidurapu12@gmail.com>
2024-08-26 20:28:48 +0300

    cors issue fixed

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -3,10 +3,24 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const allowedOrigins = [
+    'https://abz-agency-test-task-ui.vercel.app',
+    'http://abz-agency-test-task-ui.vercel.app',
+    'http://localhost:5173',
+  ];
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization', // 'http://localhost:5173',
+    credentials: true,
   });
   await app.listen(3000);
 }

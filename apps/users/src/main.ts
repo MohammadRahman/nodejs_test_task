8e2b34136436ad9e4316995178b8c7684ad3c835
mohammadrahman <obaidurapu12@gmail.com>
2024-08-26 20:47:49 +0300

    allowed all origins in the backend

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -3,24 +3,10 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const allowedOrigins = [
-    'https://abz-agency-test-task-ui.vercel.app',
-    'http://abz-agency-test-task-ui.vercel.app',
-    'http://localhost:5173',
-  ];
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept, Authorization', // 'http://localhost:5173',
-    credentials: true,
+    origin: '*',
   });
   await app.listen(3000);
 }
